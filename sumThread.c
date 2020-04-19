9076dd81ba3f33bd6a232e14f238c31c413c19d2
Christian Dunham <dunhamc@uw.edu>
2020-04-19 11:15:50 -0700

    complete

--- sumThread.c
+++ sumThread.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 //sum computed by backgruond thread
-long long sum = 0;
-
+struct sum_runner_struct
+{
+  long long limit;
+  long long answer;
+};
 /* thread function sums 0 to N */
 void* sum_runner(void* arg)
 {
   //cast the void
-  long long *limit_ptr = (long long*) arg;
-  long long limit = *limit_ptr;
-
-  for (long long i = 0; i <= limit; i++)
+  struct sum_runner_struct *arg_struct = (struct sum_runner_struct*) arg;
+  long long sum = 0;
+  for (long long i = 0; i <= arg_struct->limit; i++)
   {
     sum += i;
   }//end for sum
-
+  
+  arg_struct->answer = sum;
   //sum is global var so threads can access
   pthread_exit(0);//exit
 }//end thread runner
@@ -24,23 +27,31 @@ int main(int argc, char **argv)
 {
   if (argc < 2)
   {
-    printf("Usage: %s <num>\n", argv[0]);
+    printf("Usage: %s <num 1> <num 2> ...<num -n>\n", argv[0]);
     exit(-1);
   }//end if wrong num args
-  
-  long long limit = atoll(argv[1]);
+  int num_args = argc - 1;//arg count minus name of file 
 
+   struct sum_runner_struct args[num_args];
  //thread id
- pthread_t tid;
+ pthread_t tids[num_args];
 
- //create attributes
- pthread_attr_t attr;
- pthread_attr_init(&attr);
 
- pthread_create(&tid, &attr, sum_runner, &limit);
+ for (int i = 0; i < num_args; i++)
+ {
+   args[i].limit = atoll(argv[i + 1]);
+   //create attributes
+   pthread_attr_t attr;
+   pthread_attr_init(&attr);
+   pthread_create(&tids[i], &attr, sum_runner, &args[i]);
+ }//
+ 
   //could do other work here...
  //dont exit immediately.. wait till thred works
- pthread_join(tid, NULL);
- printf("Sum is %lld\n",sum);//long long decimal
+ for (int i = 0; i < num_args; i++)
+ {
+   pthread_join(tids[i], NULL);
+   printf("Sum for thread %d is %lld\n",i, args[i].answer);//long long decimal
+ }
 
 }//end main
